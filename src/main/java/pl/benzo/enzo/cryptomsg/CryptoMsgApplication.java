e4748b8500dcab6f83294f5ce1537f74af42694a
KubaBart <256466@student.pwr.edu.pl>
2023-09-09 18:34:20 +0200

    init project

---
+++ src/main/java/pl/benzo/enzo/cryptomsg/CryptoMsgApplication.java
@@ -0,0 +1,13 @@
+package pl.benzo.enzo.cryptomsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoMsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoMsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/benzo/enzo/cryptomsg/CryptoMsgApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.benzo.enzo.cryptomsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoMsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
