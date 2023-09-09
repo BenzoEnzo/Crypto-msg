9a3ff0b74f1daf9dbd525c3cfe1d952ae85eb661
KubaBart <256466@student.pwr.edu.pl>
2023-09-10 00:00:21 +0200

    Swagger init

--- src/main/java/pl/benzo/enzo/cryptomsg/web/MsgController.java
+++ src/main/java/pl/benzo/enzo/cryptomsg/web/MsgController.java
@@ -1,4 +1,39 @@
 package pl.benzo.enzo.cryptomsg.web;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.benzo.enzo.cryptomsg.web.api.MsgApi;
+import pl.benzo.enzo.cryptomsg.web.mechanism.MsgService;
+import pl.benzo.enzo.cryptomsg.web.transform.dto.request.CreateMsgRequest;
+import pl.benzo.enzo.cryptomsg.web.transform.dto.request.ReadMsgRequest;
+import pl.benzo.enzo.cryptomsg.web.transform.dto.response.CreateMsgResponse;
+import pl.benzo.enzo.cryptomsg.web.transform.dto.response.ReadMsgResponse;
+
+@RestController
+@RequestMapping("/api/msg")
 public class MsgController {
+    private final MsgService msgService;
+    public MsgController(MsgService msgService) {
+        this.msgService = msgService;
+    }
+    @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CreateMsgResponse> create(@RequestBody CreateMsgRequest createMsgRequest) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body( msgService.createCryptoMessage(createMsgRequest));
+    }
+
+    @PostMapping(value = "/read", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ReadMsgResponse> read(@RequestBody ReadMsgRequest readMsgRequest) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(msgService.readCryptoMessage(readMsgRequest));
+
+    }
 }
