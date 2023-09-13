46c7e6bbbfc3bcfebcf42dfc436cef4b499cce30
KubaBart <256466@student.pwr.edu.pl>
2023-09-13 18:49:24 +0200

    crypto-key verify-token fix mono

--- src/main/java/pl/benzo/enzo/cryptomsg/external/configuration/CryptoKeyClient.java
+++ src/main/java/pl/benzo/enzo/cryptomsg/external/configuration/CryptoKeyClient.java
@@ -4,12 +4,9 @@ package pl.benzo.enzo.cryptomsg.external.configuration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import pl.benzo.enzo.cryptomsg.external.KeyApi;
 
@@ -40,7 +37,9 @@ public class CryptoKeyClient implements KeyApi {
     }
 
     public ResponseEntity<Boolean> validateKey(String encryptedKey){
-        return webClient.get()
+        return webClient.post()
+                .uri("/verify")
+                .bodyValue(encryptedKey)
                 .retrieve()
                 .bodyToMono(Boolean.class)
                 .map(responseBody -> ResponseEntity.ok().body(responseBody))
