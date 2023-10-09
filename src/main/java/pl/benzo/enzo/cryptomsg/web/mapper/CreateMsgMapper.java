7b11ca3680d4af3f680338063054e2c3b1035d62
= <=>
2023-10-09 18:04:52 +0200

    refactor mapper's code

--- src/main/java/pl/benzo/enzo/cryptomsg/web/mapper/CreateMsgMapper.java
+++ src/main/java/pl/benzo/enzo/cryptomsg/web/mapper/CreateMsgMapper.java
@@ -4,14 +4,12 @@ import pl.benzo.enzo.cryptomsg.web.entity.Msg;
 import pl.benzo.enzo.cryptomsg.web.dto.request.CreateMsgRequest;
 import pl.benzo.enzo.cryptomsg.web.dto.response.CreateMsgResponse;
 
-public class CreateMsgMapper implements Mapper<CreateMsgRequest,CreateMsgResponse> {
+public class CreateMsgMapper {
 
-    @Override
     public Msg requestMapper(CreateMsgRequest createMsgRequest) {
         return new Msg(createMsgRequest.content(),createMsgRequest.deleteAfterTime());
     }
 
-    @Override
     public CreateMsgResponse responseMapper(Msg msg) {
         return new CreateMsgResponse(msg.getId(),msg.getSendAt());
     }
--- src/main/java/pl/benzo/enzo/cryptomsg/web/mapper/ReadMsgMapper.java
+++ src/main/java/pl/benzo/enzo/cryptomsg/web/mapper/ReadMsgMapper.java
@@ -4,13 +4,7 @@ import pl.benzo.enzo.cryptomsg.web.entity.Msg;
 import pl.benzo.enzo.cryptomsg.web.dto.request.ReadMsgRequest;
 import pl.benzo.enzo.cryptomsg.web.dto.response.ReadMsgResponse;
 
-public class ReadMsgMapper implements Mapper<ReadMsgRequest, ReadMsgResponse> {
-
-    @Override
-    public Msg requestMapper(ReadMsgRequest readMsgRequest) {
-        return null;
-    }
-    @Override
+public class ReadMsgMapper {
     public ReadMsgResponse responseMapper(Msg msg) {
         return new ReadMsgResponse(msg.getContent(),msg.getOpenAt(),msg.isSuccess());
     }
